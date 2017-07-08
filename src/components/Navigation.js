286d212235b32cc207bc481c5aae4b045e90f85f
Katherine Sampias <ksampias@gmail.com>
2017-07-07 17:58:16 -0700

    Added react-bootstrap module and navigation bar.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 
 class Navigation extends Component {
-  render() {
-    return (
-      <div>
-         <p>NAV BAR HERE</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Navbar inverse collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">MetroTourist</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+
+                        <Nav pullRight>
+                            <NavItem eventKey={1} href="#">Video Search</NavItem>
+                            <NavItem eventKey={2} href="#">Tour Listings</NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 export default Navigation;
