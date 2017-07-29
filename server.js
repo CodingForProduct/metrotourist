e735b172efa27fe21620260527168ac3ec0fb765
Katherine Sampias <ksampias@gmail.com>
2017-07-28 19:37:35 -0700

    Removed test routes from server.js

--- server.js
+++ server.js
@@ -46,33 +46,6 @@ db.once('open', function () {
   console.log('Mongoose connection successful.');
 });
 
-// Routes
-
-app.get('/', function (req, res) {
-    res.status(200).send('Hello World!');
-});
-
-//array of tours -- 
-const tourOptions = [
-  { id: 1, name: "Hollywood", destinations: ["HardRock", "Chinese Theater", "Walk of Stars"]},
-  { id: 2, name: "Downtown LA", destinations: ["Pershing Square", "Angel's Flight"]},
-  { id: 3, name: "Santa Monica", destinations: ["The Pier", "3rd Street Promenade"] },
-  { id: 4, name: "Culver City", destinations: ["Culver Hotel", "The Culver City Stairs", "Baldwin Hills Scenic Overlook"]},
-];
-
-// display list of tours
-app.get('/tours', function (request, response) {
-    response.status(200)
-  .send(tourOptions);
-});
-
-app.get('/add', function (req, res) {
-    res.status(200)
-    .send({
-        sum: parseInt(req.query.a) + parseInt(req.query.b)
-    });
-});
-
 // initialize api routes
 app.use('/api', require('./routes/api_routes'));
 app.use('/api/tests', tests);
